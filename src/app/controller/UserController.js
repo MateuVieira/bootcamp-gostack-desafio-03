13a6062b306e284bd42032ba4879ba7ab6f26476
MateuVieira <mateus12v@hotmail.com>
2020-02-02 16:16:12 -0300

    Add UserController store

--- src/app/controller/UserController.js
+++ src/app/controller/UserController.js
@@ -1,8 +1,38 @@
+import * as Yup from 'yup';
+
 import User from '../models/User';
 
 class UserController {
   async store(req, res) {
-    return res.json();
+    const schema = Yup.object().shape({
+      name: Yup.string()
+        .required()
+        .min(4),
+      email: Yup.string()
+        .email()
+        .required(),
+      password: Yup.string()
+        .required()
+        .min(6),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Verify faild.' });
+    }
+
+    const userExist = await User.findOne({ where: { email: req.body.email } });
+
+    if (userExist) {
+      return res.status(401).json({ error: 'User already exist.' });
+    }
+
+    const { id, name, email } = await User.create(req.body);
+
+    return res.json({
+      id,
+      name,
+      email,
+    });
   }
 }
 
