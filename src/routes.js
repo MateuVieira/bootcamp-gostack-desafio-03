11a733de92e41a9b74d55d02498e529db984fc29
MateuVieira <mateus12v@hotmail.com>
2020-02-02 16:04:47 -0300

    Add AuthMiddleware

--- src/routes.js
+++ src/routes.js
@@ -3,9 +3,13 @@ import { Router } from 'express';
 import UserController from './app/controller/UserController';
 import SessionController from './app/controller/SessionController';
 
+import authMiddleware from './app/middleware/auth';
+
 const routes = new Router();
 
 routes.post('/sessions', SessionController.store);
+
+routes.use(authMiddleware);
 
 routes.post('/users', UserController.store);
 
