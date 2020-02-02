86fbda5b5be55939b478374aef7879ab488be4b6
MateuVieira <mateus12v@hotmail.com>
2020-02-02 16:52:10 -0300

    Add UserController update

--- src/app/controller/UserController.js
+++ src/app/controller/UserController.js
@@ -34,6 +34,50 @@ class UserController {
       email,
     });
   }
+
+  async update(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().min(4),
+      email: Yup.string().email(),
+      old_password: Yup.string().min(6),
+      password: Yup.string()
+        .min(6)
+        .when('old_password', (old_password, field) =>
+          old_password ? field.required() : field
+        ),
+      confirm_password: Yup.string().when('password', (password, field) =>
+        password ? field.required().oneOf([Yup.ref('password')]) : field
+      ),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation faild.' });
+    }
+
+    const user = await User.findByPk(req.userId);
+
+    const { email, old_password } = req.body;
+
+    if (email && email !== user.email) {
+      const userExist = await User.findOne({ where: { email } });
+
+      if (userExist) {
+        return res.status(401).json({ error: 'User already exist' });
+      }
+    }
+
+    if (old_password && !(await user.checkPassword(old_password))) {
+      return res.status(401).json({ error: 'Password does not match' });
+    }
+
+    const { id, name } = await user.update(req.body);
+
+    return res.json({
+      id,
+      name,
+      email,
+    });
+  }
 }
 
 export default new UserController();
