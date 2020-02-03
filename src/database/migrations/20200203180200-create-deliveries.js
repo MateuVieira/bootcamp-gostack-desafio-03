0ce2bd92ed26a8bd2bfb65282920251d0f2efa83
MateuVieira <mateus12v@hotmail.com>
2020-02-03 15:10:56 -0300

    Add Migration deliveries

---
+++ src/database/migrations/20200203180200-create-deliveries.js
@@ -0,0 +1,57 @@
+module.exports = {
+  // eslint-disable-next-line prettier/prettier
+  up: (queryInterface, Sequelize) => queryInterface.createTable('deliveries',
+    {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      product: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      repicient_id: {
+        type: Sequelize.INTEGER,
+        references: { model: 'repicients', key: 'id' },
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL',
+        allowNull: true,
+      },
+      deliveryman_id: {
+        type: Sequelize.INTEGER,
+        references: { model: 'deliverymans', key: 'id' },
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL',
+        allowNull: true,
+      },
+      signature_id: {
+        type: Sequelize.INTEGER,
+        references: { model: 'files', key: 'id' },
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL',
+        allowNull: true,
+      },
+      canceled_at: {
+        type: Sequelize.DATE,
+      },
+      start_date: {
+        type: Sequelize.DATE,
+      },
+      end_date: {
+        type: Sequelize.DATE,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    }),
+
+  // eslint-disable-next-line arrow-parens
+  down: (queryInterface) => queryInterface.dropTable('deliveries'),
+};
