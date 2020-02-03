cceecda156f1e2f9725d35c0b70675cf258cb99f
MateuVieira <mateus12v@hotmail.com>
2020-02-03 09:10:09 -0300

    Add DeliverymanController

--- src/routes.js
+++ src/routes.js
@@ -6,6 +6,7 @@ import UserController from './app/controller/UserController';
 import SessionController from './app/controller/SessionController';
 import FileController from './app/controller/FileController';
 import RepicientsController from './app/controller/RepicientsController';
+import DeliverymanController from './app/controller/DeliverymanController';
 
 import authMiddleware from './app/middleware/auth';
 
@@ -24,6 +25,8 @@ routes.get('/repicients/:id', RepicientsController.show);
 routes.post('/repicients', RepicientsController.store);
 routes.put('/repicients', RepicientsController.update);
 routes.delete('/repicients/:id', RepicientsController.delete);
+
+routes.post('/deliverymans', DeliverymanController.store);
 
 routes.post('/files', upload.single('file'), FileController.store);
 
