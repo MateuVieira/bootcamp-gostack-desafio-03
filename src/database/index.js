0166b38423516ffeefd8fd04b2f3ef0a86cad337
MateuVieira <mateus12v@hotmail.com>
2020-02-04 17:41:10 -0300

    Add model DeliveryProblems

--- src/database/index.js
+++ src/database/index.js
@@ -7,8 +7,9 @@ import File from '../app/models/File';
 import Repicient from '../app/models/Repicient';
 import Deliveryman from '../app/models/Deliveryman';
 import Delivery from '../app/models/Delivery';
+import DeliveryProblems from '../app/models/DeliveryProblems';
 
-const models = [User, File, Repicient, Deliveryman, Delivery];
+const models = [User, File, Repicient, Deliveryman, Delivery, DeliveryProblems];
 
 class Database {
   constructor() {
